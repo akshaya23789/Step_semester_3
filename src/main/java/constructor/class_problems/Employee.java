182aaff03a1c0f74746d72f1fc7df954ad175da7
akshaya23789 <akshaya23789@gmail.com>
2026-09-04 23:25:21 +0530

    m2

---
+++ src/main/java/constructor/class_problems/Employee.java
@@ -0,0 +1,24 @@
+package constructor.class_problems;
+public class Employee {
+    String empId;
+    double salary;
+    public Employee(String empId, double salary) {
+        this.empId = empId;
+        this.salary = salary;
+    }
+    public void raiseSalary(double salary) {
+        this.salary += salary;
+    }
+    public static void main(String[] args) {
+        Employee[] employees = {
+            new Employee("E-101", 40000),
+            new Employee("E-102", 55000),
+            new Employee("E-103", 62000),
+            new Employee("E-104", 48000)
+        };
+        for (int i = 0; i < employees.length; i++) {
+            employees[i].raiseSalary(5000);
+            System.out.println(employees[i].empId + " | Final Salary: Rs " + employees[i].salary);
+        }
+    }
+}
